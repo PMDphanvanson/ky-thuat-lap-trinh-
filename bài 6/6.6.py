cb07180f0a328e0fb64019760850105532e28377
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 22:30:46 +0700

    Create 6.6.py

---
+++ bài 6/6.6.py
@@ -0,0 +1,20 @@
+print ("Họ tên: Phan văn sơn")
+print ("MSSV: 235752021610098")
+
+class Chuoi:
+    def __init__(self):
+        self.string = ""
+
+    def get_String(self):
+       
+        self.string = input("Nhập chuỗi: ")
+
+    def print_String(self):
+       
+        print("Chuỗi in hoa:", self.string.upper())
+        
+chuoi = Chuoi()
+
+chuoi.get_String()
+
+chuoi.print_String()
