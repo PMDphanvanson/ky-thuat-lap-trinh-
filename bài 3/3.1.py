c0b53817b26b851c6909584f78f60bf62da18a98
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 21:20:58 +0700

    Create 3.1.py

---
+++ bài 3/3.1.py
@@ -0,0 +1,9 @@
+print ("Ho ten: Phan văn sơn")
+print ("MSSV: 235752021610098")
+
+def sum(a, b):
+    
+    print("Tong hai so a va b la: " +str(a+b))
+a=int(input("Nhap a: "))
+b=int(input("Nhap b: "))
+sum(a,b)
