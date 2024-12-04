6067425abdb7b9547da7847d3973acf1b2913de2
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 21:59:38 +0700

    Create 4.11.py

---
+++ bài 4/4.11.py
@@ -0,0 +1,7 @@
+print ("Ho ten: Phan văn sơn")
+print ("MSSV: 235752021610098")
+
+ds = input('Nhap chuoi thu nhat: ').split()
+them_ds = input('Nhap chuoi thu hai: ').split()
+ds.append(them_ds)
+print ("Danh sách sau khi đã thêm phần tử: " ,ds)
