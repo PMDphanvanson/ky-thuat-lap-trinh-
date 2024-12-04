b1b557dd572eea9be9a8fac4b94a4c98bf76eef1
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 22:16:03 +0700

    Create 5.4.py

---
+++ bài 5/5.4.py
@@ -0,0 +1,11 @@
+print("phanvawn sơn")
+print("235752021610098")
+import numpy as np
+
+array = np.arange(12, 39)
+print("Mảng được tạo:")
+print(array)
+reversed_array = array[::-1]
+
+print("Mảng đảo ngược:")
+print(reversed_array)
