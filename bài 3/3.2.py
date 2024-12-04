8e0cc762219a745ec1b3df4f2c1aa29772ed0d35
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 21:23:20 +0700

    Update 3.2.py

--- bài 3/3.2.py
+++ bài 3/3.2.py
@@ -1,1 +1,10 @@
+print ("Ho ten: Phan văn sơn")
+print ("MSSV: 235752021610098")
 
+def sum(a, b):
+    return a + b
+
+a = int(input("Nhap a: "))
+b = int(input("Nhap b: "))          
+c = sum (a,b)
+print("Tong cua" ,a, "va" ,b, "a:  " +str(c))
