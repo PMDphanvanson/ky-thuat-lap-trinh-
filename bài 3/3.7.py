b62bde13b6f8d8bc2bbcdf588ffd643e378bda40
PMDphanvanson <phanvanson140105@gmail.com>
2024-12-04 21:38:12 +0700

    Create 3.7.py

---
+++ bài 3/3.7.py
@@ -0,0 +1,12 @@
+print ("Ho ten: Phan văn sơn")
+print ("MSSV: 235752021610098")
+
+def checkValue(n): 
+    if n%2 == 0: 
+        print ("Số" ,n, "là số chẵn") 
+    else: 
+        print ("Số", n ,"là số lẻ") 
+n = int(input("Nhap n: "))
+checkValue(n)
+
+
